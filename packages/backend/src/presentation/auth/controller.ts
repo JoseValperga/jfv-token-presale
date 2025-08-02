e955ca13df91678b7d232075e556e1ffa9fb8369
JoseValperga <jose.valperga@gmail.com>
2025-08-02 20:26:46 +0000

    Casos de uso listos

--- packages/backend/src/presentation/auth/controller.ts
+++ packages/backend/src/presentation/auth/controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { AuthRepository, CustomError, RegisterUserDto } from "../../domain";
+import {
+  AuthRepository,
+  CustomError,
+  RegisterUserDto,
+  RegisterUser,
+} from "../../domain";
 import { JwtAdapter } from "../../config";
 import { UserModel } from "../../data/potgresdb";
 
@@ -20,14 +25,9 @@ export class AuthController {
 
     if (error) return res.status(400).json({ error });
 
-    this.authRepository
-      .register(registerUserDto!)
-      .then(async (user) =>
-        res.json({
-          user,
-          token: await JwtAdapter.generateToken({ id: user.id }),
-        })
-      )
+    new RegisterUser(this.authRepository)
+      .execute(registerUserDto!)
+      .then((data) => res.json(data))
       .catch((error) => this.handleError(error, res));
   };
 
